e85bcaa52c6e4d18a44ffd1df9e0ec2550821492
Javinix96 <113954291+Javinix96@users.noreply.github.com>
2023-06-25 19:26:17 -0600

    app

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sahred-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [],
+})
+export class SidebarComponent {}
